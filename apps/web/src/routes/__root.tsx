d5c2650f3cb648204c7ab826280eabd6862a1f5f
Natsume-197 <jonathan.197ariza@gmail.com>
2026-06-13 19:13:57 -0500

    feat: better font for japanese text

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -68,7 +68,7 @@ export const Route = createRootRouteWithContext<RouterAppContext>()({
 			},
 			{
 				rel: "stylesheet",
-				href: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=DM+Serif+Display&display=swap",
+				href: "https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap",
 			},
 			{
 				rel: "stylesheet",
